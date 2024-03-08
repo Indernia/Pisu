ae36de60b7b83fc14d2288000195077b5a2451a5
Alex Lundberg <lundberg.alex1@gmail.com>
2024-03-08 14:04:54 +0100

    customised gear direction

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/TurnGear.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/TurnGear.java
@@ -2,8 +2,20 @@ package dk.dtu.compute.se.pisd.roborally.model;
 
 
 class TurnGear implements SpecialSpace{
-    
-    private String direction = "left";
+    private String direction;
+
+    public TurnGear(){
+        this.direction = "left";    
+    }
+
+    public TurnGear(String direction){
+        if (direction.toLowerCase() == "left" || direction.toLowerCase() == "right"){
+            this.direction = direction.toLowerCase();
+        }
+        else {
+            this.direction = "left";
+        }
+    }
 
     public void triggerEffect(Player player){
         if (direction == "left"){
