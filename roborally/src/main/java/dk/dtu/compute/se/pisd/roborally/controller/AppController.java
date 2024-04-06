a9c1cdafe93ea3b0aa69203578858f638c0650fe
Alex Lundberg <lundberg.alex1@gmail.com>
2024-04-06 14:58:49 +0200

    Simple save to db

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -28,6 +28,8 @@ import dk.dtu.compute.se.pisd.roborally.RoboRally;
 
 import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.dal.IRepository;
+import dk.dtu.compute.se.pisd.roborally.dal.RepositoryAccess;
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
@@ -77,7 +79,10 @@ public class AppController implements Observer {
             // XXX the board should eventually be created programmatically or loaded from a
             // file
             // here we just create an empty board with the required number of players.
-            Board board = new Board(8, 8);
+            //Board board = new Board(8, 8);
+
+            Board board = BoardFactory.getInstance().createBoard(null);
+
             gameController = new GameController(board);
             int no = result.get();
             for (int i = 0; i < no; i++) {
@@ -89,13 +94,20 @@ public class AppController implements Observer {
             // XXX: V2
             board.setCurrentPlayer(board.getPlayer(0));
             gameController.startProgrammingPhase();
+            
+            RepositoryAccess.getRepository().createGameInDB(board);
 
             roboRally.createBoardView(gameController);
         }
     }
 
     public void saveGame() {
-        // XXX needs to be implemented eventually
+
+        if (gameController != null  && gameController.board.getGameId() != null) {
+            RepositoryAccess.getRepository().updateGameInDB(gameController.board);
+        }
+
+
     }
 
     public void loadGame() {
