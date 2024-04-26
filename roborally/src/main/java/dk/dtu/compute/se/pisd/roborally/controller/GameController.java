d8039d109970674114930716cfdef33f8c92d417
Lars Nissen <larsnissen@Larss-MacBook-Pro.local>
2024-04-26 14:20:59 +0200

    now also able to handle if it is an option card

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -408,7 +408,16 @@ public class GameController {
         int currentReg = board.getStep();
         CommandCard topCard = player.drawCard();
         player.setProgramField(currentReg, topCard);
+        if(topCard.command != Command.OPTION_LEFT_RIGHT){
         executeCommand(player, topCard.command);
+        } else {
+            double random = Math.random();
+            if(random < 0.5){
+                executeCommand(player,Command.RIGHT);
+            } else {
+                executeCommand(player, Command.LEFT);
+            }
+        }
     }
 
     public boolean moveCards(@NotNull CommandCardField source, @NotNull CommandCardField target) {
