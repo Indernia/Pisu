eead9ad6014cdab8aaff62f9f7fbf24f4a10284a
Alex Lundberg <lundberg.alex1@gmail.com>
2024-05-05 21:28:23 +0200

    changing git ignore

---
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/dal/DBAcces.java
@@ -0,0 +1,19 @@
+// Purpose: This file contains the database access information.
+
+package dk.dtu.compute.se.pisd.roborally.dal;
+
+/*
+ * This class contains the database access information. 
+ *
+ * @author Alex Lundberg (235442)
+ *
+ */
+public class DBAcces {
+
+    public static final String USER = "root";
+    public static final String PASSWORD = "password";
+    public static final String dbName = "PISU";
+    public static final int Port = 3306;
+    public static final String Host = "localhost";
+}
+
