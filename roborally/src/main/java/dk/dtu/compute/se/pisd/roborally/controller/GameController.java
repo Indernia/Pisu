5da2fe518f4b48709e6ae22c2b6973d45a8ce656
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2024-05-21 08:10:18 +0200

    fix

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -481,10 +481,10 @@ public class GameController {
      */
     public void spamDamage(@NotNull Player player) {
         int currentReg = board.getStep();
+        player.discardCard(player.getProgramField(currentReg).getCard());
         player.setProgramField(currentReg, player.drawCard());
         CommandCard topCard = player.getProgramField(currentReg).getCard();
         if(topCard != null){
-        player.discardCard(topCard);
         if (topCard.command != Command.OPTION_LEFT_RIGHT) {
             executeCommand(player, topCard.command);
         } else {
